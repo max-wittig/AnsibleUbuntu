bd9f42b1ba69bbaa197fa3074fcdac921b022f43
Max Wittig <max.wittig95@gmail.com>
2018-11-11 16:27:11 +0100

    refactor: a lot of improvements and fixes

---
+++ terraform/README.md
@@ -0,0 +1,5 @@
+# Deploy terraform
+
+1. Generate a new API token at digitalocean
+1. TF_VAR_do_token=sometoken terraform plan 
+1. Get domain name
---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+resource "digitalocean_droplet" "web" {
+  image  = "39342610" # ubuntu-18.10
+  name = "ubuntu-18.10"
+  region = "fra1"
+  size = "s-1vcpu-1gb"
+  monitoring = "false"
+  ssh_keys = ["23597109", "20943250", "20980721"]
+}
