4f2dde1bdff3dae034727e800aba13ccfe44cf75
Max Wittig <max.wittig95@gmail.com>
2019-11-15 21:31:34 +0100

    feat: handle DNS records with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,31 @@ resource "digitalocean_domain" "default" {
   name       = "maxwittig.xyz"
   ip_address = digitalocean_droplet.web.ipv4_address
 }
+
+resource "digitalocean_record" "A" {
+  domain = digitalocean_domain.default.name
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_droplet.web.ipv4_address
+}
+
+resource "digitalocean_record" "do1" {
+  domain = digitalocean_domain.default.name
+  type   = "NS"
+  name   = "@"
+  value  = "ns1.digitalocean.com."
+}
+
+resource "digitalocean_record" "do2" {
+  domain = digitalocean_domain.default.name
+  type   = "NS"
+  name   = "@"
+  value  = "ns2.digitalocean.com."
+}
+
+resource "digitalocean_record" "do3" {
+  domain = digitalocean_domain.default.name
+  type   = "NS"
+  name   = "@"
+  value  = "ns3.digitalocean.com."
+}
