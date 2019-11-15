bc0c01164946416e8d93b96c4b8366a1be6fe982
Max Wittig <max.wittig95@gmail.com>
2019-11-15 19:09:27 +0100

    chore(terraform): upgrade to 0.12

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,21 @@
-variable "do_token" {}
+variable "do_token" {
+}
 
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = var.do_token
 }
 
 resource "digitalocean_droplet" "web" {
-  image  = "39342610" # ubuntu-18.10
-  name = "ubuntu-18.10"
-  region = "fra1"
-  size = "s-1vcpu-1gb"
+  image      = "39342610" # ubuntu-18.10
+  name       = "ubuntu-18.10"
+  region     = "fra1"
+  size       = "s-1vcpu-1gb"
   monitoring = "false"
-  ssh_keys = ["23597109", "20943250", "20980721"]
+  ssh_keys   = ["23597109", "20943250", "20980721"]
 }
 
 resource "digitalocean_domain" "default" {
   name       = "maxwittig.xyz"
-  ip_address = "${digitalocean_droplet.web.ipv4_address}"
+  ip_address = digitalocean_droplet.web.ipv4_address
 }
+
